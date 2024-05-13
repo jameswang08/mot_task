5bb799d3eb47d626712e90e9c1fbc9bbb5ef83af
James Wang <114769089+jameswang08@users.noreply.github.com>
2024-05-13 12:22:38 -0700

    feat(main.py): add displayTxt helper function for text display and input capture
    This function will display an inputted message to user and if we want to collect input, it will listen for the 0 or 1 keypress.

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from psychopy import visual, core
+from psychopy import visual, core, event
 from trial import Trial
+
+#Helper functions
+
+#display text to user and capture input if needed
+def displayTxt(window, message, input = False, size = 1):
+    txt = visual.TextStim(window, text=message, color='black',font='Helvetica', 
+    units = 'deg', height=size, wrapWidth=size*50)
+    txt.draw(window)
+    window.flip()
+    keys = None
+    if not input:
+        event.waitKeys()
+    else:
+        keys = event.waitKeys(keyList=['0', '1'])
+    return keys
 
 def main():
     window = visual.Window([1280, 800], allowGUI=True, monitor='test', color='white', fullscr=False)
-    test_trial = Trial(window, 4, ['red']*4, 20, 10, 1)
+    test_trial = Trial(window, 4, ['blue']+['red']*3, 20, 10, 1)
     test_trial.display()
+    test_trial.eventLoop()
     core.wait(10)
 
 if __name__ == "__main__":
